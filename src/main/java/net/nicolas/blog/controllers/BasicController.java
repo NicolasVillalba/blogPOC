5ce295fd4bbb9c55e928c09f62388310c25f36b6
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-23 08:21:34 -0300

    added two endpoints to handles posts requests

--- src/main/java/net/nicolas/blog/controllers/BasicController.java
+++ src/main/java/net/nicolas/blog/controllers/BasicController.java
@@ -1,9 +1,15 @@
 package net.nicolas.blog.controllers;
 
+import net.nicolas.blog.dtos.PostDTO;
 import net.nicolas.blog.dtos.ResponseDTO;
 import net.nicolas.blog.services.NamesService;
+import net.nicolas.blog.services.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +18,25 @@ public class BasicController {
     @Autowired
     private NamesService nameService;
 
+    @Autowired
+    private PostsService postsService;
+
     @GetMapping("/")
     public ResponseDTO home(){
         return new ResponseDTO("message", nameService.get());
+    }
+
+    @PostMapping("/")
+    public ResponseDTO createPost(@RequestBody PostDTO post){
+        postsService.save(post);
+        return new ResponseDTO("message", "posted");
+    }
+
+    @PostMapping("/enhance/post")
+    public ResponseEntity<ResponseDTO> createPostEnhanced(@RequestBody PostDTO post){
+        postsService.save(post);
+         return ResponseEntity
+                 .status(HttpStatus.CREATED)
+                 .body(new ResponseDTO("message", "posted"));
     }
 }
