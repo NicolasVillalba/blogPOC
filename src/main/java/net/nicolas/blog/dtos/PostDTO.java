63685570af157f83156322ff7f0e2a0f5816abe0
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-23 08:19:46 -0300

    added DTO to handle body request

---
+++ src/main/java/net/nicolas/blog/dtos/PostDTO.java
@@ -0,0 +1,49 @@
+package net.nicolas.blog.dtos;
+
+public class PostDTO {
+    private String title;
+    private String state;
+    private String text;
+    private String imagePath;
+    private String authorId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+}
