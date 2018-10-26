a37fd4a3c8f4144b8b5d2366e05aab4a2fc4098b
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-26 02:34:11 -0300

    added new method using with sql queries

--- src/main/java/net/nicolas/blog/repositories/PostsRepository.java
+++ src/main/java/net/nicolas/blog/repositories/PostsRepository.java
@@ -3,6 +3,7 @@ package net.nicolas.blog.repositories;
 import net.nicolas.blog.domain.Author;
 import net.nicolas.blog.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
 @Repository
 public interface PostsRepository  extends JpaRepository<Post, Long> {
     Post findById(Long i);
-    List<Post> findByAuthor(Author i);
+
+    @Query("select p from Post p where p.author.id = ?1")
+    List<Post> findAuthorPostsByID(Long author_id);
+
+    @Query("select p from Post p where p.title like %?1%")
+    List<Post> searchPostsTitles(String query);
 }
