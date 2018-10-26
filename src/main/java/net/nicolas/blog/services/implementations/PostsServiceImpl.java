500ba23beb1cc97c7d74f743beafb7e43c000ebf
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-26 02:42:37 -0300

    added some fixes to match the new design of entities & getting rid of autowire in favor of a better way to inject dependencies

--- src/main/java/net/nicolas/blog/services/implementations/PostsServiceImpl.java
+++ src/main/java/net/nicolas/blog/services/implementations/PostsServiceImpl.java
@@ -6,21 +6,25 @@ import net.nicolas.blog.domain.Post;
 import net.nicolas.blog.dtos.PostDTO;
 import net.nicolas.blog.repositories.PostsRepository;
 import net.nicolas.blog.services.PostsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostsServiceImpl implements PostsService {
-    @Autowired
+
     private PostsRepository postsRepository;
+
+    public PostsServiceImpl(PostsRepository postsRepository){
+        this.postsRepository = postsRepository;
+    }
 
     @Override
     public void save(PostDTO postDTO) {
         Post p = new Post(postDTO.getTitle(), null);
+        Author a = new Author();
         Content c = new Content(postDTO.getText());
         p.setDraft(true);
         p.setContent(c);
-        p.setAuthor(new Author());
+        p.setAuthor(a);
         postsRepository.save(p);
     }
 }
