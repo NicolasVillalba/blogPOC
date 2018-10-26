1632fc5a6c3fe92ff7164987afe322cf9a5cfb4f
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-26 02:29:12 -0300

    added new methods to handle more precisely the data that has to be generated

--- src/main/java/net/nicolas/blog/services/FakerService.java
+++ src/main/java/net/nicolas/blog/services/FakerService.java
@@ -1,6 +1,15 @@
 package net.nicolas.blog.services;
 
+/**
+ * Responsible for creating dummy data
+ * to feed the data base to make tests
+ */
 public interface FakerService {
     String fullName();
     String sportsHeadLine();
+    String textLine();
+    String paragraphText(Integer n);
+    String anyWord();
+    Integer anyDigit();
+    String imageUrls();
 }
--- src/main/java/net/nicolas/blog/services/implementations/FakerServiceImp.java
+++ src/main/java/net/nicolas/blog/services/implementations/FakerServiceImp.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Service;
 
 import com.github.javafaker.Faker;
 
+/**
+ * Formal Implementation that uses
+ * javaFaker lib
+ */
 @Service
 public class FakerServiceImp implements FakerService {
 
@@ -18,5 +22,30 @@ public class FakerServiceImp implements FakerService {
     public String sportsHeadLine() {
         String s = faker.esports().game() + faker.esports().player();
         return String.format("%s1 %s2", faker.esports().game(), faker.esports().player());
+    }
+
+    @Override
+    public String textLine() {
+        return faker.lorem().characters(15);
+    }
+
+    @Override
+    public String paragraphText(Integer n) {
+        return faker.lorem().paragraph(n);
+    }
+
+    @Override
+    public String anyWord() {
+        return faker.beer().name();
+    }
+
+    @Override
+    public Integer anyDigit() {
+        return faker.number().randomDigit();
+    }
+
+    @Override
+    public String imageUrls() {
+        return faker.avatar().image();
     }
 }
