3d4ca77c301c6280693ca53e923cf29091a0ac01
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-26 02:33:25 -0300

    added improvements in the seed code, get rid of Autowired annotation

--- src/main/java/net/nicolas/blog/listeners/DataSeederListener.java
+++ src/main/java/net/nicolas/blog/listeners/DataSeederListener.java
@@ -1,5 +1,7 @@
 package net.nicolas.blog.listeners;
 
+import net.nicolas.blog.domain.Author;
+import net.nicolas.blog.domain.Content;
 import net.nicolas.blog.domain.Post;
 import net.nicolas.blog.repositories.PostsRepository;
 import net.nicolas.blog.services.FakerService;
@@ -13,17 +15,34 @@ import java.time.LocalDateTime;
 @Component
 public class DataSeederListener {
 
-    @Autowired
     private PostsRepository postsRepository;
 
-    @Autowired
     private FakerService fakerService;
+
+    public DataSeederListener(PostsRepository postsRepository, FakerService fakerService){
+        this.postsRepository = postsRepository;
+        this.fakerService = fakerService;
+    }
 
     @EventListener
     public void seedDataOnH2(ContextRefreshedEvent event){
         System.out.println(":::::::::::::::::SEEDING DB::::::::::::::::::::");
         for (int i = 0; i < 20; i++){
             Post p = new Post(fakerService.sportsHeadLine(), LocalDateTime.now());
+            Author author = new Author();
+            author.setFullName(fakerService.fullName());
+            author.setAbout(fakerService.paragraphText(1));
+
+            Content content = new Content(fakerService.paragraphText(2));
+            content.getImages().add(fakerService.imageUrls());
+            content.getImages().add(fakerService.imageUrls());
+            content.getImages().add(fakerService.imageUrls());
+            p.setAuthor(author);
+            p.setContent(content);
+            p.getTags().add(fakerService.anyWord());
+            p.getTags().add(fakerService.anyWord());
+            p.getTags().add(fakerService.anyWord());
+            p.setRating(fakerService.anyDigit());
             postsRepository.save(p);
         }
     }
