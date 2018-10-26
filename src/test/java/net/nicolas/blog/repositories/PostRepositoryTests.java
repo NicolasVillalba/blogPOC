5af97877575e95f13e5b033255fbac7b926d6b24
Nicolas Villalba <nerynicolas@gmail.com>
2018-10-26 02:43:57 -0300

    added tests for the new methods of Post repo

--- src/test/java/net/nicolas/blog/repositories/PostRepositoryTests.java
+++ src/test/java/net/nicolas/blog/repositories/PostRepositoryTests.java
@@ -1,7 +1,7 @@
 package net.nicolas.blog.repositories;
 
 import net.nicolas.blog.domain.Post;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,27 +20,41 @@ public class PostRepositoryTests {
 
     @Test
     public void findAllPostsTest(){
-        Assert.assertNotNull(postRepo.findAll());
+        assertNotNull(postRepo.findAll());
     }
 
     @Test
     public void saveAPostTest(){
         postRepo.save(new Post("My Post", LocalDateTime.now()));
-        Assert.assertTrue(postRepo.findAll().size() <= 21);
+        assertTrue(postRepo.findAll().size() <= 21);
     }
 
     @Test
     public void postTitleTest(){
         Post p = postRepo.findById(1L);
-        Assert.assertTrue(p.getTitle().length() > 0);
-        Assert.assertTrue(p.getContent().getText().length() > 0);
+        assertTrue(p.getTitle().length() > 0);
+        assertTrue(p.getContent().getText().length() > 0);
     }
 
     @Test
     public void authorFullNameTest(){
         Post p = postRepo.findById(5L);
-        Assert.assertNotNull(p.getAuthor().getFullName());
-        Assert.assertTrue(p.getAuthor().getFullName().length() > 0);
+        assertNotNull(p.getAuthor().getFullName());
+        assertTrue(p.getAuthor().getFullName().length() > 0);
     }
 
+    @Test
+    public void searchPostsByAuthorID(){
+        List<Post> posts = postRepo.findAuthorPostsByID(3L);
+        System.out.println(posts.get(0));
+        assertNotNull("Repository has some thing",posts);
+        assertTrue("At least one element",posts.size() > 0);
+    }
+
+    @Test
+    public void serachPostsByTitle(){
+        List<Post> posts = postRepo.searchPostsTitles("as");
+        System.out.println(posts.size());
+        assertNotNull(posts);
+    }
 }
