541455ac9e5af253df592b54dae2b8c22eb7ab3d
Nicolas <nerynicolas@gmail.com>
2019-01-21 15:33:55 -0300

    added configuration for viewControllers and resources handling

--- src/main/java/net/nicolas/blog/BlogApplication.java
+++ src/main/java/net/nicolas/blog/BlogApplication.java
@@ -2,11 +2,31 @@ package net.nicolas.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(scanBasePackages = {"net.nicolas.blog"})
-public class BlogApplication {
+@EnableWebMvc
+public class BlogApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/ajax/fetch").setViewName("publish_post_fetch");
+		registry.addViewController("/ajax").setViewName("publish_post_ajax");
+		registry.addViewController("/ajax/jquery").setViewName("publish_post_jquery");
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/resources/**")
+				.addResourceLocations("classpath:/static/");
+	}
+
 }
